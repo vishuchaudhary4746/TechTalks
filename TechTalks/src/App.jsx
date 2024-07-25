f8a52f829ebd90e68e8c860d9f27980b5ecd0cbd
vishuchaudhary4746 <vishuchaudhary4746@gmail.com>
2024-07-26 00:17:24 +0530

    Created Blog app

---
+++ TechTalks/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import React from 'react'
+import Home from './Components/Home/Home.jsx'
+
+function App() {
+
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default App
---
+++ TechTalks/src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+
+      <div className="navbar-collapse">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Home <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Link
+            </a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" role="button">
+              Dropdown
+            </a>
+            <div className="dropdown-menu">
+              <a className="dropdown-item" href="#">
+                Action
+              </a>
+              <a className="dropdown-item" href="#">
+                Another action
+              </a>
+              <div className="dropdown-divider"></div>
+              <a className="dropdown-item" href="#">
+                Something else here
+              </a>
+            </div>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link disabled" href="#">
+              Disabled
+            </a>
+          </li>
+        </ul>
+        <form className="form-inline">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+          />
+          <button className="btn btn-outline-success" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
